5f17d29eaf11bcc2744a34718b2190c495b70d24
tuanpv62 <phantuan62.paster@gmail.com>
2024-03-05 21:29:38 +0700

    update UI page Address

--- FE-dev/Orchid-web/app/(platform)/order-history/_components/_lib_components/Grid_Address_Default.tsx
+++ FE-dev/Orchid-web/app/(platform)/order-history/_components/_lib_components/Grid_Address_Default.tsx
@@ -4,8 +4,7 @@ import { AlertOctagon, Terminal } from "lucide-react";
 
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
-
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -14,10 +13,10 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import GetApi_Province from "@/app/(platform)/order/_components/_lib_component/GetApi_Province";
 
 function Grid_Address_Default() {
   const handleSubmit = () => {};
@@ -66,24 +65,11 @@ function Grid_Address_Default() {
               <div className="grid grid-cols-1 items-center gap-4 ">
                 <div className="flex items-center justify-center">
                   <div className="relative w-full">
-                    <Input
-                      id="username"
-                      name="username"
-                      type="text"
-                      className="border-b col-span-4 border-gray-300 py-1 focus:border-b-2 focus:border-blue-700 transition-colors focus:outline-none peer bg-inherit"
-                    />
-                    <label
-                      htmlFor="username"
-                      className="absolute left-0 top-1 cursor-text peer-focus:text-xs peer-focus:-top-4 transition-all peer-focus:text-blue-700 px-2 text-gray-500"
-                    >
-                      Tỉnh/Thành Phố, Quận/Huyện, Phường/Xã
-                    </label>
+                    <GetApi_Province />
                   </div>
                 </div>
               </div>
-              <div className="grid grid-cols-4 items-center gap-4 ">
-              
-              </div>
+              <div className="grid grid-cols-4 items-center gap-4 "></div>
             </div>
             <DialogFooter>
               <Button type="submit">Save changes</Button>
@@ -91,45 +77,131 @@ function Grid_Address_Default() {
           </DialogContent>
         </Dialog>
       </div>
+      {/* defalut  */}
       <div className="py-2">
         <Alert className="flex flex-col">
           <div className="flex justify-between items-center">
             <Terminal className="h-4 w-4" />
-            <AlertTitle className="mr-auto">Kain Shun</AlertTitle>
+            <AlertTitle className="">Kain Shun</AlertTitle>
+            <div className="border-1  h-full border-black ml-1 mr-1"> |</div>
 
-            <AlertDescription>test</AlertDescription>
+            <AlertTitle className="mr-auto ">(+84) 394581438</AlertTitle>
             <AlertDescription className="ml-auto ">
-              You can add components to your app using the cli.
+              <button className="cursor-pointer text-blue-400 bg-white relative inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-[#F5F5F5] hover:text-[#60A5FA] h-9 rounded-md px-3">
+                Cập nhật
+              </button>
             </AlertDescription>
           </div>
 
-          <div>
-            <AlertDescription className="mb-auto">qwertyuyiou</AlertDescription>
+          <div className="flex items-center">
+            <AlertDescription className="order-1 mr-auto">
+              left infomation
+            </AlertDescription>
+            <AlertDescription className="order-2 ml-auto">
+              <button className="disabled text-gray-500 border-2-gray-500 cursor-not-allowed bg-white relative inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-[#F5F5F5] hover:text-[#60A5FA] h-9 rounded-md px-3">
+                Thiết lập mặc định
+              </button>
+            </AlertDescription>
           </div>
           <div>
-            <AlertDescription className="mb-auto">qwertyuyiou</AlertDescription>
-          </div>
-          <div>
-            <AlertDescription className="mb-auto">qwertyuyiou</AlertDescription>
+            <AlertDescription className="mb-auto">
+              <label
+                className=" bg-white relative inline-flex items-center 
+              justify-center gap-2 text-sm font-medium
+                 h-9 rounded-md px-3 border border-red-500 text-red-600"
+              >
+                Mặc định
+              </label>
+            </AlertDescription>
           </div>
         </Alert>
       </div>
+
+      {/* new address  */}
+
       <div className="py-2">
-        <Alert className="flex justify-between items-center">
-          <Terminal className="h-4 w-4" />
-          <AlertTitle className="mr-auto">Heads up!</AlertTitle>
-          <AlertDescription className="ml-auto ">
-            You can add components to your app using the cli.
-          </AlertDescription>
+        <Alert className="flex flex-col">
+          <div className="flex justify-between items-center">
+            <Terminal className="h-4 w-4" />
+            <AlertTitle className="">Kain Shun</AlertTitle>
+            <div className="border-1  h-full border-black ml-1 mr-1"> |</div>
+
+            <AlertTitle className="mr-auto ">(+84) 394581438</AlertTitle>
+            <AlertDescription className="ml-auto ">
+              <button className="cursor-pointer text-blue-400 bg-white relative inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-[#F5F5F5] hover:text-[#60A5FA] h-9 rounded-md px-3">
+                Cập nhật
+              </button>
+              <button className="cursor-pointer text-blue-400 bg-white relative inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-[#F5F5F5] hover:text-[#60A5FA] h-9 rounded-md px-3">
+                Xóa
+              </button>
+            </AlertDescription>
+          </div>
+
+          <div className="flex items-center">
+            <AlertDescription className="order-1 mr-auto">
+              left infomation
+            </AlertDescription>
+            <AlertDescription className="order-2 ml-auto">
+              <button className="cursor-pointer bg-white relative inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-[#F5F5F5] hover:text-[#60A5FA] h-9 rounded-md px-3">
+                Thiết lập mặc định
+              </button>
+            </AlertDescription>
+          </div>
+          <div>
+            <AlertDescription className="mb-auto">
+              <label
+                className=" bg-white relative inline-flex items-center 
+              justify-center gap-2 text-sm font-medium
+                 h-9 rounded-md px-3 border border-red-500 text-red-600"
+              >
+                Mặc định
+              </label>
+            </AlertDescription>
+          </div>
         </Alert>
       </div>
+
+      {/* new address  */}
+
       <div className="py-2">
-        <Alert className="flex justify-between items-center">
-          <Terminal className="h-4 w-4" />
-          <AlertTitle className="mr-auto">Heads up!</AlertTitle>
-          <AlertDescription className="ml-auto ">
-            You can add components to your app using the cli.
-          </AlertDescription>
+        <Alert className="flex flex-col">
+          <div className="flex justify-between items-center">
+            <Terminal className="h-4 w-4" />
+            <AlertTitle className="">Kain Shun</AlertTitle>
+            <div className="border-1  h-full border-black ml-1 mr-1"> |</div>
+
+            <AlertTitle className="mr-auto ">(+84) 394581438</AlertTitle>
+            <AlertDescription className="ml-auto ">
+              <button className="cursor-pointer text-blue-400 bg-white relative inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-[#F5F5F5] hover:text-[#60A5FA] h-9 rounded-md px-3">
+                Cập nhật
+              </button>
+              <button className="cursor-pointer text-blue-400 bg-white relative inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-[#F5F5F5] hover:text-[#60A5FA] h-9 rounded-md px-3">
+                Xóa
+              </button>
+            </AlertDescription>
+          </div>
+
+          <div className="flex items-center">
+            <AlertDescription className="order-1 mr-auto">
+              left infomation
+            </AlertDescription>
+            <AlertDescription className="order-2 ml-auto">
+              <button className="cursor-pointer bg-white relative inline-flex items-center justify-center gap-2 rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-[#F5F5F5] hover:text-[#60A5FA] h-9 rounded-md px-3">
+                Thiết lập mặc định
+              </button>
+            </AlertDescription>
+          </div>
+          <div>
+            <AlertDescription className="mb-auto">
+              <label
+                className=" bg-white relative inline-flex items-center 
+              justify-center gap-2 text-sm font-medium
+                 h-9 rounded-md px-3 border border-red-500 text-red-600"
+              >
+                Mặc định
+              </label>
+            </AlertDescription>
+          </div>
         </Alert>
       </div>
     </>
